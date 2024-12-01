f8f66728745e0be1f2a8a604a9c7ee2d4b6590e1
Joshua Joseph <jo634076@ucf.edu>
2024-11-30 23:51:58 -0500

    Cleaned up repository and added a new test case. Updated the README.md and the testSuite Accordingly

--- setNonsense.py
+++
@@ -1,9 +0,0 @@
-from collections import defaultdict
-thisset = set()
-thisset.add(14)
-thisset.add(22)
-thisset.add(10)
-print(thisset)
-wocky = 10
-if wocky in thisset:
-  print("Yes, 'apple' is in this set")
--- testSuite.sh
+++ testSuite.sh
@@ -7,6 +7,7 @@ python3 main.py testInputs/test4.pla testOutputs/testOutput4.pla
 python3 main.py testInputs/test5.pla testOutputs/testOutput5.pla
 python3 main.py testInputs/test6.pla testOutputs/testOutput6.pla
 python3 main.py testInputs/test7.pla testOutputs/testOutput7.pla
+python3 main.py testInputs/test8.pla testOutputs/testOutput8.pla
 echo "Finished generating data. Comparing results..."
 diff -iw testOutputs/testOutput1.pla validationOutputs/output1.pla
 echo "Test 1 Complete"
@@ -22,4 +23,6 @@ diff -iw testOutputs/testOutput6.pla validationOutputs/output6.pla
 echo "Test 6 Complete"
 diff -iw testOutputs/testOutput7.pla validationOutputs/output7.pla
 echo "Test 7 Complete"
+diff -iw testOutputs/testOutput8.pla validationOutputs/output8.pla
+echo "Test 8 Complete"
 echo "Finished running test suite"
